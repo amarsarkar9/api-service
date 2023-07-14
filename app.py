dcfdc9ca374464bd91fc4d998bb7141dc9824935
amar sarkar <amarsarkar9@gmail.com>
2023-07-14 19:38:35 +0530

    cng

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import render_template, request, jsonify, Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-	return "welcome to the flask tutorial"
 
+@app.route('/', methods=["GET"])
+def hello():
+    # return "welcome to the flask tutorial"
+    return render_template("/index.html")
 
 if __name__ == "__main__":
-	app.run(host ='0.0.0.0', port = 5001, debug = True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+# import pymongo
+
+# client = pymongo.MongoClient("mongodb+srv://amarsarkar:qwertyuiop@cluster0.f0ztixh.mongodb.net/test")
+# database=client["amar"]
+# col=database["sample"]
+# doc=col.insert_one({"name":"amar", "_id":"1"})
+# col.delete_one({"name":"amar"})
+# database.drop_collection(col)
---
+++ services/connection.py
@@ -0,0 +1,22 @@
+# from pymongo import MongoClient
+#
+# # Replace the placeholders with your MongoDB connection string and database name
+# connection_string = "mongodb+srv://amarsarkar:qwertyuiop@cluster0.f0ztixh.mongodb.net/?retryWrites=true&w=majority"
+#
+#
+# # Create a MongoClient object
+# client = MongoClient(connection_string,27017)
+#
+#
+# database_name = "admin"
+#
+# # Database name
+# db = client[database_name]
+#
+# # Collection name
+# col = db[database_name].drop()
+#
+# # drop collection col1
+# print(col.drop())
+#
+#
