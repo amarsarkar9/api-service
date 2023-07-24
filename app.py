307d792ec60c9807b9225d218df5e9857245af3a
Amar Sarkar <amar.sarkar@us.mcd.com>
2023-07-24 17:48:43 +0530

    check

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def hello():
-    # return "welcome to the flask tutorial"
-    return render_template("/index.html")
+    return "welcome"
+    # return render_template("/index.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
